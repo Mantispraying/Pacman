1f0fe7a7bf8d5855359c69f9eb90240df39467c0
Mantispraying <srajanskgupta@gmail.com>
2021-12-01 03:18:45 +0530

    Minor adjustments

--- app.js
+++ app.js
@@ -234,7 +234,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // Check for a win
   function checkForWin() {
     if (score >= 274) {
-      ghost.forEach(ghost => clearInterval(ghost.timerId))
+      ghosts.forEach(ghost => clearInterval(ghost.timerId))
       document.removeEventListener('keyup', movePacman)
       scoreDisplay.innerHTML = "YOU WIN!"
     }
